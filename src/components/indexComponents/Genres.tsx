7755570bcb7a1222f4c2af105e7563935604de70
BD-Git-Hub <bovindesouza@gmail.com>
2022-07-19 13:52:31 +0100

    added custom spacing to tailwind.config, tailwind

--- src/components/indexComponents/Genres.tsx
+++ src/components/indexComponents/Genres.tsx
@@ -2,27 +2,9 @@ import styled from "styled-components";
 import ArrowButton from "../button/ArrowButton";
 import Items from "./Items/Items";
 
-const GenresContainer = styled.div`
-margin-top: 1rem;
-  background-color: blue;
-  height: 30rem;
-  width: 100%;
-`;
 
-const LeftArrowDiv = styled.div`
-  background-color: orange;
-  width: 8%;
-  height: 30rem;
-  display: inline-block;
-`;
 
-const LeftArrowButton = styled.button`
-  position: relative;
-  top: 40%;
-  font-size: 100%;
-  float: left;
-  margin: 0;
-`;
+
 
 const CenterDiv = styled.div`
   background-color: pink;
@@ -50,20 +32,33 @@ const RightArrowButton = styled.button`
 
 const Genres = () => {
   return (
-    <GenresContainer>
-      <LeftArrowDiv>
-        <LeftArrowButton>Left Arrow</LeftArrowButton>
-        {/* <ArrowButton name={"Left Arrow"} direction={left}/> */}
-      </LeftArrowDiv>
-      <CenterDiv>
-        <Items />
-      </CenterDiv>
 
-      <RightArrowDiv>
-        <RightArrowButton>Right Arrow</RightArrowButton>
-        {/* <ArrowButton name={"right Arrow"} direction={right}/> */}
-      </RightArrowDiv>
-    </GenresContainer>
+    <div className="bg-blue-500 h-96 w-full flex flex-row">
+      <div className="bg-orange-400 h-96 basis-36">
+        <button className="relative top-2/4 text-lg float-left">Left Arrow</button>
+      </div>
+      <div className="bg-pink-400 h-96 basis-11/12">
+        {/* <Items/> */}
+      </div>
+      <div className="bg-orange-400 h-96 basis-36">
+        <button className="relative top-2/4 text-lg float-right">Right Arrow</button>
+      </div>
+    </div>
+
+    // <GenresContainer>
+    //   <LeftArrowDiv>
+    //     <LeftArrowButton>Left Arrow</LeftArrowButton>
+    //     {/* <ArrowButton name={"Left Arrow"} direction={left}/> */}
+    //   </LeftArrowDiv>
+    //   <CenterDiv>
+    //     <Items />
+    //   </CenterDiv>
+
+    //   <RightArrowDiv>
+    //     <RightArrowButton>Right Arrow</RightArrowButton>
+    //     {/* <ArrowButton name={"right Arrow"} direction={right}/> */}
+    //   </RightArrowDiv>
+    // </GenresContainer>
   );
 };
 
